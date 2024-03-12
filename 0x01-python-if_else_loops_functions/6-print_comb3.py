d9e7d4fe8cdd21bd00ed03fe95d5740e4f3ecc5b
mark2wavy254 <muturimark16@gmail.com>
2024-03-12 19:35:15 +0300

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for m in range(48, 57):
     for n in range(49, 58):
-        print("{}{}, ".format(chr(m), chr(n)), end='')
+        if n > m:
+            print("{}{}, ".format(chr(m), chr(n)), end='')
         if m == 56 and n == 57:
             print("{}{}".format(chr(m), chr(n)), end='\n')
