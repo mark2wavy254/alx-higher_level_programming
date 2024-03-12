30867f96a34db58d20bc7183aa42d29f514cc57e
mark2wavy254 <muturimark16@gmail.com>
2024-03-12 19:06:22 +0300

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for m in range(0, 100):
-    print("{:02d}, ".format(int(m)), end='')
-    if m == 99:
-        print("", end='\n')
+    if m < 99:
+        print("{:02d}, ".format(int(m)), end='')
+    else:
+        print("{:02d}".format(int(m)), end='\n')
