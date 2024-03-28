cc3181dcb917d86c990355103790d16f2ec8dbf5
mark2wavy254 <muturimark16@gmail.com>
2024-03-28 15:07:18 +0300

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,6 +14,6 @@ if a == 1:
     print(f"1: {sys.argv[0]}")
 
 elif a > 1:
-    print("{a} arguments:")
+    print(f"{a} arguments:")
     for i, arg in enumerate(sys.argv[1:], start=1):
         print(f"{i}: {arg}")
