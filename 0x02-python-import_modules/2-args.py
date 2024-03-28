64193d9613a80448745237c3c847b991ddbaa4b3
mark2wavy254 <muturimark16@gmail.com>
2024-03-28 15:12:08 +0300

    corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,11 +7,9 @@ a = len(sys.argv) - 1
 if a == 0:
     print("0 arguments. ")
 
-print(f"Number of argments: {a}")
-
 if a == 1:
     print("1 argument:")
-    print(f"1: {sys.argv[0]}")
+    print(f"1: {sys.argv[1]}")
 
 elif a > 1:
     print(f"{a} arguments:")
