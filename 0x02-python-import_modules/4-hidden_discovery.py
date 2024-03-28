6833a27133ec03c47afd34cf21d0ff9d7986d80c
mark2wavy254 <muturimark16@gmail.com>
2024-03-28 16:37:26 +0300

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
-
-
 if __name__ == "__main__":
     import hidden_4
 
-    
     a = dir(hidden_4)
-    fn = [name for name in a if not
-            name.startswith('__')]
+
+    fn = [name for name in a if not name.startswith('__')]
+
     for name in sorted(fn):
         print(name)
-
