a6b8819bab8e46b43022cca39812d8692c572a0a
mark2wavy254 <muturimark16@gmail.com>
2024-04-03 17:36:17 +0300

    task6correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for m in range(0, 10):
     for n in range(m + 1, 10):
-        if m == 8 and n == 9:
-            print('89')
-        else:
+        if m < 8 and n < 9:
             print('{}{}, '.format(m,n), end='')
+        elif m == 8 and n == 9:
+            print('89')
