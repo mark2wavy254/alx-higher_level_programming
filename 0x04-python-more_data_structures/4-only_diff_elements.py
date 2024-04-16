05b02bfde3cc62052cc311dc3576573e0078be22
mark2wavy254 <muturimark16@gmail.com>
2024-04-16 17:18:11 +0300

    task4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    df_set = set()
+    for i in set_1:
+        if not i in set_2:
+            df_set.add(i)
+    for i in set_2:
+        if not i in set_1:
+            df_set.add(i)
+    return df_set
