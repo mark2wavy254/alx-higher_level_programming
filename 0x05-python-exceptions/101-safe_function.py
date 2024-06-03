206a3075edad1e31f0baf6988a4e2c3e0866ca61
mark2wavy254 <muturimark16@gmail.com>
2024-06-03 17:45:37 +0300

    task8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except (RuntimeError, ZeroDivisionError, TypeError, IndexError, ValueError) as r:
+    except (ZeroDivisionError, IndexError) as r:
         print("Exception:", r, file=sys.stderr)
         return None
