cb8d6d83e6a2e7c351fd8660b670540f2af91304
mark2wavy254 <muturimark16@gmail.com>
2024-06-03 17:32:12 +0300

    task7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value), end='\n')
+        return True
+    except (TypeError, ValueError) as x:
+        print("Exception: ", x, file=sys.stderr)
+        return False
