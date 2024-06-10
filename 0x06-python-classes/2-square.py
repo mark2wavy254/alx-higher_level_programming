cf91bb3de59f409d437fe0ebc713663f3d61f666
mark2wavy254 <muturimark16@gmail.com>
2024-06-10 15:31:38 +0300

    task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a square with a private instance attribute"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initializor
+        Args
+            size: length of the side of a square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
