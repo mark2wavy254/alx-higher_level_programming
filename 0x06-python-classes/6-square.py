07e302de95accbdc8fa6f278af66ad88b4337ddc
mark2wavy254 <muturimark16@gmail.com>
2024-06-11 09:08:07 +0300

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,6 +60,6 @@ class Square:
 
         [print("", end="") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for k in range(0, self.__position[0])]
-            [print("#", end="") for j in range(0, self.__size)]
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
